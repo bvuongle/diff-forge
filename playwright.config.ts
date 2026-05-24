f44c1b6eaaeb54b62872575e916c0ef4b295e2d7
Binh Vuong Le Duc <ducbinhhn2002@gmail.com>
2026-05-24 20:37:07 +0200

    fix(test): increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   outputDir: './tests/e2e/test-results',
-  timeout: 60_000,
+  timeout: 90_000,
   retries: 0,
   fullyParallel: false,
   reporter: process.env.CI ? 'github' : 'list',
